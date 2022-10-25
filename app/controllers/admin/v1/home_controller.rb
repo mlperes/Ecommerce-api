84255d4d929cdd72544ef16dc3c587f8a812fc4d
Marcelo Peres <peresonrails@gmail.com>
2022-10-25 20:42:16 -0300

    some configurations on devise gem

--- app/controllers/admin/v1/home_controller.rb
+++ app/controllers/admin/v1/home_controller.rb
@@ -1,5 +1,5 @@
 module Admin::V1
-  class  HomeController > ApiController
+  class  HomeController < ApiController
     def index
       render json: {message: 'teste!!'}
     end
--- app/controllers/storefront/v1/api_controller.rb
+++ app/controllers/storefront/v1/api_controller.rb
@@ -1,4 +1,4 @@
-class Storefront::V1
+moduleStorefront::V1
   class ApiController < ApplicationController
     include Authenticable
   end
